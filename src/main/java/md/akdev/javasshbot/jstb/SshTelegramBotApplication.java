57ed2d35ed247f575779dbd6c5f224ce00d9d4cb
Alex Kols <kolosoff.alex@gmail.com>
2022-04-03 00:39:39 +0300

    JSTB-0:Added clean spring boot project

---
+++ src/main/java/md/akdev/javasshbot/jstb/SshTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package md.akdev.javasshbot.jstb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/akdev/javasshbot/jstb/SshTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package md.akdev.javasshbot.jstb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
